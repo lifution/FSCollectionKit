1ad4417c552930b6c8245c609997a4df81459773
VincentLee <kingofthenorth@foxmail.com>
2024-11-08 18:04:02 +0800

    Add FSCollectionViewFlowLayout: Support tap gesture which can avoid elements automatically for collection view.

---
+++ Source/Classes/CollectionViewLayouts/FSCollectionViewLayout.swift
@@ -0,0 +1,76 @@
+//
+//  FSCollectionViewLayout.swift
+//  FSCollectionKit
+//
+//  Created by VincentLee on 2024/11/8.
+//
+
+import UIKit
+import Foundation
+
+open class FSCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    // MARK: Properties/Open
+    
+    open var isTapEnabled = false
+    open var onDidTapHandler: ((_ tap: UITapGestureRecognizer) -> Void)?
+    
+    // MARK: Properties/Private
+    
+    private let tap = UITapGestureRecognizer()
+    private let tapDelegator = _TapGestureDelegator()
+    
+    // MARK: Initialization
+    
+    override public init() {
+        super.init()
+        p_didInitialize()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        p_didInitialize()
+    }
+    
+    // MARK: Override
+    
+    open override func prepare() {
+        super.prepare()
+        if let view = collectionView, view !== tap.view {
+            view.addGestureRecognizer(tap)
+        }
+    }
+    
+    // MARK: Private
+    
+    private func p_didInitialize() {
+        tapDelegator.layout = self
+        tap.delegate = tapDelegator
+        tap.addTarget(self, action: #selector(p_tapAction))
+    }
+    
+    @objc
+    private func p_tapAction() {
+        onDidTapHandler?(tap)
+    }
+    
+    fileprivate func p_gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard isTapEnabled, let view = collectionView else {
+            return false
+        }
+        if let attributes = layoutAttributesForElements(in: view.bounds) {
+            let point = gestureRecognizer.location(in: view)
+            if let _ = attributes.first(where: { $0.frame.contains(point) }) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+private final class _TapGestureDelegator: NSObject, UIGestureRecognizerDelegate {
+    weak var layout: FSCollectionViewFlowLayout?
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return layout?.p_gestureRecognizerShouldBegin(gestureRecognizer) ?? true
+    }
+}
--- Source/Classes/CollectionViewLayouts/InsetGroupLayout/CollectionInsetGroupLayout.swift
+++ Source/Classes/CollectionViewLayouts/InsetGroupLayout/CollectionInsetGroupLayout.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-open class CollectionInsetGroupLayout: UICollectionViewFlowLayout {
+open class CollectionInsetGroupLayout: FSCollectionViewFlowLayout {
     
     // MARK: Properties/Open
     
